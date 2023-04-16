c96b00875ad0808f57fb433cdb379fa48fb2726f
FIRST_NAME <MY_NAME@example.com>
2023-04-16 11:52:34 +0530

    Maven Project

---
+++ src/test/java/testcases/HomeLoanTest.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HomeLoanTest extends BaseClass {
+	
+	
+	@Test(dependsOnMethods = "test2")
+	public void test1() {
+		System.out.println("Inside home loan test 1");
+	}
+
+	@Test(groups = {"sanity" , "regression"})
+	public void test2() {
+		System.out.println("Inside home loan test 2");
+	}
+	
+	
+}
